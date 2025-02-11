32a5576f59fe2549328823691745a26f82b98f97
salmamakina <salma.makinaa2557@gmail.com>
2025-02-11 16:23:10 +0100

    updating install-drill-zookeeper.sh

--- install-drill-zookeeper.sh
+++ install-drill-zookeeper.sh
@@ -3,36 +3,35 @@
 # Variables
 DIR=/opt
 REPO_GITHUB=/tmp/repo_installation
-$role=$1
+role=$1
 
 
 if ! [ -f ./tar/apache-zookeeper-3.9.2-bin.tar.gz ]; then
   echo "zookeeper Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/zookeeper/zookeeper-3.9.2/apache-zookeeper-3.9.2-bin.tar.gz
+  wget -P ./tar https://dlcdn.apache.org/zookeeper/zookeeper-3.9.3/apache-zookeeper-3.9.3.tar.gz
 fi
 
 if ! [ -f ./tar/apache-drill-1.21.1.tar.gz ]; then
   echo "Drill Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/drill/1.21.1/apache-drill-1.21.1.tar.gz
+  wget -P ./tar https://dlcdn.apache.org/drill/1.21.2/apache-drill-1.21.2.tar.gz
 fi
 
-
 echo  " Untaring zookeeper to kepler folder ... "
-tar -zxf ./tar/apache-zookeeper-3.9.2-bin.tar.gz --directory $DIR
+tar -zxf ./tar/apache-zookeeper-3.9.3.tar.gz --directory $DIR
 echo  " Untaring drill to kepler folder ... "
-tar -zxf ./tar/apache-drill-1.21.1.tar.gz --directory $DIR
+tar -zxf ./tar/apache-drill-1.21.2.tar.gz --directory $DIR
 mv $DIR/apache-zookeeper* $DIR/zookeeper
 mv $DIR/apache-drill* $DIR/drill
 
 sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-drill/drill-override.conf
 rm /opt/drill/conf/drill-override.conf
-cp $REPO_GITHUB/config-drill/drill-override.conf /opt/drill/conf/
-cp $REPO_GITHUB/config-drill/zoo.cfg /opt/zookeeper/conf/ 
-apt install -y net-tools
-apt install -y netcat 
+sudo cp $REPO_GITHUB/config-drill/drill-override.conf /opt/drill/conf/
+sudo cp $REPO_GITHUB/config-drill/zoo.cfg /opt/zookeeper/conf/ 
+# apt install -y net-tools
+# apt install -y netcat 
 
-cp $REPO_GITHUB/$role-systemd/drill.service /etc/systemd/system/
-cp $REPO_GITHUB/$role-systemd/zookeeper.service /etc/systemd/system/
+sudo cp $REPO_GITHUB/$role-systemd/drill.service /etc/systemd/system/
+sudo cp $REPO_GITHUB/$role-systemd/zookeeper.service /etc/systemd/system/
 
 # Enabling services 
 echo "Enabling service files ... "
