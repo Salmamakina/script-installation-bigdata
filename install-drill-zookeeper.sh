6f3fdb6e40da02b9da469e5691f98ac458ad8404
salmamakina <salma.makinaa2557@gmail.com>
2025-02-13 12:20:19 +0100

    setting up the systemd

--- install-drill-zookeeper.sh
+++ install-drill-zookeeper.sh
@@ -7,53 +7,53 @@ MASTER_IP_ADDRESS=$1
 WORKER_IP_ADDRESS=$2
 role=$3
 
-if ! [ -f ./tar/apache-zookeeper-3.9.2-bin.tar.gz ]; then
-  echo "zookeeper Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/zookeeper/zookeeper-3.9.3/apache-zookeeper-3.9.3-bin.tar.gz
-fi
+# if ! [ -f ./tar/apache-zookeeper-3.9.2-bin.tar.gz ]; then
+#   echo "zookeeper Binary isn't found downloading ... "
+#   wget -P ./tar https://dlcdn.apache.org/zookeeper/zookeeper-3.9.3/apache-zookeeper-3.9.3-bin.tar.gz
+# fi
 
-if ! [ -f ./tar/apache-drill-1.21.1.tar.gz ]; then
-  echo "Drill Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/drill/1.21.2/apache-drill-1.21.2.tar.gz
-fi
+# if ! [ -f ./tar/apache-drill-1.21.1.tar.gz ]; then
+#   echo "Drill Binary isn't found downloading ... "
+#   wget -P ./tar https://dlcdn.apache.org/drill/1.21.2/apache-drill-1.21.2.tar.gz
+# fi
 
-echo  " Untaring zookeeper to kepler folder ... "
-tar -zxf ./tar/apache-zookeeper-3.9.3-bin.tar.gz --directory $DIR
-echo  " Untaring drill to kepler folder ... "
-tar -zxf ./tar/apache-drill-1.21.2.tar.gz --directory $DIR
-mv $DIR/apache-zookeeper* $DIR/zookeeper
-mv $DIR/apache-drill* $DIR/drill
+# echo  " Untaring zookeeper to kepler folder ... "
+# tar -zxf ./tar/apache-zookeeper-3.9.3-bin.tar.gz --directory $DIR
+# echo  " Untaring drill to kepler folder ... "
+# tar -zxf ./tar/apache-drill-1.21.2.tar.gz --directory $DIR
+# mv $DIR/apache-zookeeper* $DIR/zookeeper
+# mv $DIR/apache-drill* $DIR/drill
 
-# configuring the addresses in drill 
-sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-drill/drill-override.conf
-sed -i "s|\${WORKER_IP_ADDRESS}|$WORKER_IP_ADDRESS|g" $REPO_GITHUB/config-drill/drill-override.conf
+# # configuring the addresses in drill 
+# sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-drill/drill-override.conf
+# sed -i "s|\${WORKER_IP_ADDRESS}|$WORKER_IP_ADDRESS|g" $REPO_GITHUB/config-drill/drill-override.conf
 
-sudo rm /opt/drill/conf/drill-override.conf
-sudo cp $REPO_GITHUB/config-drill/drill-override.conf /opt/drill/conf/
+# sudo rm /opt/drill/conf/drill-override.conf
+# sudo cp $REPO_GITHUB/config-drill/drill-override.conf /opt/drill/conf/
 
 
-# Générer le fichier zoo.cfg avec les valeurs réelles
-cat > /opt/zookeeper/conf/zoo.cfg <<EOL
-tickTime=2000
-dataDir=/var/lib/zookeeper
-dataLogDir=/var/lib/zookeeper
-clientPort=2181
-initLimit=5
-syncLimit=2
-server.0=$MASTER_IP_ADDRESS:2888:3888
-server.1=$WORKER_IP_ADDRESS:2888:3888
-EOL
+# # Générer le fichier zoo.cfg avec les valeurs réelles
+# cat > /opt/zookeeper/conf/zoo.cfg <<EOL
+# tickTime=2000
+# dataDir=/var/lib/zookeeper
+# dataLogDir=/var/lib/zookeeper
+# clientPort=2181
+# initLimit=5
+# syncLimit=2
+# server.0=$MASTER_IP_ADDRESS:2888:3888
+# server.1=$WORKER_IP_ADDRESS:2888:3888
+# EOL
 
-# Define the file myid in the zookeeper
-if [ "$role" == "master" ]; then
-    MYID=0
-else
-    MYID=1
-fi
+# # Define the file myid in the zookeeper
+# if [ "$role" == "master" ]; then
+#     MYID=0
+# else
+#     MYID=1
+# fi
 
-# Créer le répertoire s'il n'existe pas
-sudo mkdir -p /var/lib/zookeeper
-echo "$MYID" | sudo tee /var/lib/zookeeper/myid
+# # Créer le répertoire s'il n'existe pas
+# sudo mkdir -p /var/lib/zookeeper
+# echo "$MYID" | sudo tee /var/lib/zookeeper/myid
 
 apt install -y net-tools
 apt install -y netcat 
--- install-hadoop-spark.sh
+++ install-hadoop-spark.sh
@@ -8,58 +8,58 @@ role=$2
 MAX_CPU=$3
 MAX_RAM=$4
 
-# Downloading Binaries
-if ! [ -f ./tar/hadoop-3.4.0.tar.gz ]; then
-  echo "Hadoop Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz
-fi
+# # Downloading Binaries
+# if ! [ -f ./tar/hadoop-3.4.0.tar.gz ]; then
+#   echo "Hadoop Binary isn't found downloading ... "
+#   wget -P ./tar https://dlcdn.apache.org/hadoop/common/hadoop-3.4.0/hadoop-3.4.0.tar.gz
+# fi
 
-if ! [ -f ./tar/spark-3.4.3-bin-hadoop3.tgz ]; then
-  echo "Spark Binary isn't found downloading ... "
-  wget -P ./tar https://dlcdn.apache.org/spark/spark-3.5.4/spark-3.5.4-bin-hadoop3.tgz
+# if ! [ -f ./tar/spark-3.4.3-bin-hadoop3.tgz ]; then
+#   echo "Spark Binary isn't found downloading ... "
+#   wget -P ./tar https://dlcdn.apache.org/spark/spark-3.5.4/spark-3.5.4-bin-hadoop3.tgz
 
-fi
+# fi
 
-# Replacing templates Ip adresses with given Ip adresses
-sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-$role/core-site.xml
-sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-$role/yarn-site.xml
+# # Replacing templates Ip adresses with given Ip adresses
+# sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-$role/core-site.xml
+# sed -i "s|\${MASTER_IP_ADDRESS}|$MASTER_IP_ADDRESS|g" $REPO_GITHUB/config-$role/yarn-site.xml
 
-sed -i "s|\${MAX_RAM}|$MAX_RAM|g" $REPO_GITHUB/config-$role/yarn-site.xml
-sed -i "s|\${MAX_CPU}|$MAX_CPU|g" $REPO_GITHUB/config-$role/yarn-site.xml
-
-
-# Untaring Hadoop and spark Binaries
-echo  " Untaring hadoop to kepler folder ... "
-tar -zxf ./tar/hadoop-3.4.0.tar.gz --directory $DIR
-echo  " Untaring spark to kepler folder ... "
-tar -zxf ./tar/spark-3.5.4-bin-hadoop3.tgz --directory $DIR
-echo " Renaming Folders ... "
-mv $DIR/hadoop* $DIR/hadoop
-mv $DIR/spark* $DIR/spark
+# sed -i "s|\${MAX_RAM}|$MAX_RAM|g" $REPO_GITHUB/config-$role/yarn-site.xml
+# sed -i "s|\${MAX_CPU}|$MAX_CPU|g" $REPO_GITHUB/config-$role/yarn-site.xml
 
 
-# Removing default hdfs and spark on yarn config
-echo " Removing Default config files ... "
-rm $DIR/hadoop/etc/hadoop/hdfs-site.xml
-rm $DIR/hadoop/etc/hadoop/yarn-site.xml
-rm $DIR/hadoop/etc/hadoop/core-site.xml
-rm $DIR/hadoop/etc/hadoop/hadoop-env.sh
+# # Untaring Hadoop and spark Binaries
+# echo  " Untaring hadoop to kepler folder ... "
+# tar -zxf ./tar/hadoop-3.4.0.tar.gz --directory $DIR
+# echo  " Untaring spark to kepler folder ... "
+# tar -zxf ./tar/spark-3.5.4-bin-hadoop3.tgz --directory $DIR
+# echo " Renaming Folders ... "
+# mv $DIR/hadoop* $DIR/hadoop
+# mv $DIR/spark* $DIR/spark
 
-# Copying updated ip adress
-echo " Copying new config files ... "
-sudo cp $REPO_GITHUB/config-$role/yarn-site.xml $DIR/hadoop/etc/hadoop/
-sudo cp $REPO_GITHUB/config-$role/hdfs-site.xml $DIR/hadoop/etc/hadoop/
-sudo cp $REPO_GITHUB/config-$role/core-site.xml $DIR/hadoop/etc/hadoop/
-sudo cp $REPO_GITHUB/config-$role/hadoop-env.sh $DIR/hadoop/etc/hadoop/
+
+# # Removing default hdfs and spark on yarn config
+# echo " Removing Default config files ... "
+# rm $DIR/hadoop/etc/hadoop/hdfs-site.xml
+# rm $DIR/hadoop/etc/hadoop/yarn-site.xml
+# rm $DIR/hadoop/etc/hadoop/core-site.xml
+# rm $DIR/hadoop/etc/hadoop/hadoop-env.sh
+
+# # Copying updated ip adress
+# echo " Copying new config files ... "
+# sudo cp $REPO_GITHUB/config-$role/yarn-site.xml $DIR/hadoop/etc/hadoop/
+# sudo cp $REPO_GITHUB/config-$role/hdfs-site.xml $DIR/hadoop/etc/hadoop/
+# sudo cp $REPO_GITHUB/config-$role/core-site.xml $DIR/hadoop/etc/hadoop/
+# sudo cp $REPO_GITHUB/config-$role/hadoop-env.sh $DIR/hadoop/etc/hadoop/
 
 # Adding systemd services
 cp $REPO_GITHUB/$role-systemd/hadoop.service /etc/systemd/system/
 cp $REPO_GITHUB/$role-systemd/yarn.service /etc/systemd/system/
 
-# Formatting the namenode
-if [ "$role" = "master" ]; then
-    hdfs namenode -format
-fi
+# # Formatting the namenode
+# if [ "$role" = "master" ]; then
+#     hdfs namenode -format
+# fi
 
 # Enabling services 
 echo "Enabling service files ... "
