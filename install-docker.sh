fac021fb684da62cc13e1c9c73c5f80287dd5037
salmamakina <salma.makinaa2557@gmail.com>
2025-02-18 13:05:10 +0100

    updating install_docker

--- install-docker.sh
+++ install-docker.sh
@@ -6,9 +6,9 @@ REPO_GITHUB=/tmp/repo_installation
 if ! command -v docker &> /dev/null
 then
     echo "Docker n'est pas installé. Installation..."
-    sudo apt-get install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
+    apt-get install -y docker.io
+    systemctl start docker
+    systemctl enable docker
 else
     echo "Docker est déjà installé."
 fi
@@ -26,17 +26,17 @@ newgrp docker
 
 # Redémarrer Docker si nécessaire
 if sudo systemctl is-active --quiet docker; then
-    sudo systemctl restart docker
+    systemctl restart docker
 else
     echo "Docker n'est pas actif, démarrage..."
-    sudo systemctl start docker
+    systemctl start docker
 fi
 
 # Vérifier si Docker Compose est installé
 if ! command -v docker-compose &> /dev/null
 then
-  sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
+  curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 else
     echo "Docker Compose est déjà installé."
 fi
